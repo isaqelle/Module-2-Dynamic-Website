b70293b16371dea5bb40e88925f97521a3d1af11
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-05 22:23:19 +0200

    changes

---
+++ script.js
@@ -0,0 +1,12 @@
+function readMore() {
+    let moreTxt = document.getElementById("more");
+    let moreBtn = document.getElementById("moreBtn");
+
+    if (moreTxt.style.display === "none") {
+        moreTxt.style.display = "block";
+        moreBtn.innerHTML = "Read less";
+    } else {
+        moreTxt.style.display = "none";
+        moreBtn.textContent = "Read more";
+    }
+}
