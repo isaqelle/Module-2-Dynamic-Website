d2bd04e6233a4366a99aced196504fcc0900053e
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-06 18:30:25 +0200

    feat: enhance contact form with validation, update navigation links, and improve layout styles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function readMore() {
     let moreTxt = document.getElementById("more");
     let moreBtn = document.getElementById("moreBtn");
+    
 
     if (moreTxt.style.display === "none") {
         moreTxt.style.display = "block";
@@ -14,6 +15,36 @@ function readMore() {
 // FORM
 
 function validateForm() {
+    let form = document.forms["myForm"];
+
     let name = form["name"].value;
     let email = form["email"].value;
+
+    let valid = true;
+
+    if (name === "" || email === "") {
+        alert("All fields must be filled out.")
+        return false;
+    }
+
+    if (/\d/.test(name)) {
+        alert("Name can't conatin numbers.")
+        return false;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+        alert("Please enter a valid email.")
+        return false;
+    }
+
+    if (!checkbox) {
+        alert("Must confirm before submitting form.")
+        return false;
+    }
+
+    alert("Thank you for contacting us!");
+    return valid;
 }
+
+
+
