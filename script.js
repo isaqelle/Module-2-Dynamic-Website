f039723c1a45555f0b6c87a8fe01f0589a2a6c64
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-09 22:24:50 +0200

    refactor: remove unused readMore function and clean up form validation logic

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function readMore() {
-    let moreTxt = document.getElementById("more");
-    let moreBtn = document.getElementById("moreBtn");
-    
-
-    if (moreTxt.style.display === "none") {
-        moreTxt.style.display = "block";
-        moreBtn.innerHTML = "Read less";
-    } else {
-        moreTxt.style.display = "none";
-        moreBtn.textContent = "Read more";
-    }
-}
-
-// FORM
-
 function validateForm() {
     const form = document.getElementById("myFrom");
     const name = document.getElementById("name");
@@ -37,25 +21,25 @@ function validateForm() {
             missingName.append(errorTxt);
         // alert("All fields must be filled out.")
         return false;
-    }
+        }
 
-    if (/\d/.test(name)) {
-        alert("Name can't conatin numbers.")
-        return false;
-    }
+        if (/\d/.test(name)) {
+            alert("Name can't conatin numbers.")
+            return false;
+        }
 
-    if (!/\S+@\S+\.\S+/.test(email)) {
-        alert("Please enter a valid email.")
-        return false;
-    }
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            alert("Please enter a valid email.")
+            return false;
+        }
 
-    if (!checkbox) {
-        alert("Must confirm before submitting form.")
-        return false;
-    }
+        if (!checkbox) {
+            alert("Must confirm before submitting form.")
+            return false;
+        }
 
-    alert("Thank you for contacting us!");
-    return valid;
+        alert("Thank you for contacting us!");
+        return valid;
     })
     
 }
